77076a35f0aad15a4a934ff6ad82d637d091f84d
Nichole Purcell <npurcell@C1MR71JDG944.local>
2018-03-23 12:46:40 -0700

    add config files to base app

---
+++ js/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+const App = () => (
+    <div className='app'>
+        Test App
+    </div>
+)
+
+render(<App />, document.getElementById('app'))
